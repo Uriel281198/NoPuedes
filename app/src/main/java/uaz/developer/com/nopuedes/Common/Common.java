0d1247cbdaedbe0c75681bc8b926b009578c17f7
Uriel <urielarriaga.1998@gmail.com>
2018-10-13 18:29:53 -0500

    New commit for all frag

---
+++ app/src/main/java/uaz/developer/com/nopuedes/Common/Common.java
@@ -0,0 +1,17 @@
+package uaz.developer.com.nopuedes.Common;
+
+
+
+/**
+ * Created by Uriel on 03/08/2018.
+ */
+
+public class Common {
+    public static final String STR_CATEGORY_BACKGROUND = "CategoryBackground" ;
+    public static final String STR_WALPAPER = "Backgrounds";
+    public static  String CATEGORY_ID_SELECTED ;
+    public static  String CATEGORY_SELECTED;
+    public static  final int PERMISOON_REQUEST_CODE =100;
+
+
+}
